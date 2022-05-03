ae81873eadd4045b075a19564ccf30db6971c868
Alexandre Djun Igari <djun_igari@hotmail.com>
2022-05-03 12:47:02 +0900

    Add directory menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,19 +3,21 @@ import RightContent from './RightContent/RightContent'
 import SearchInput from './SearchInput'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../../firebase/clientApp';
+import Directory from './Directory/Directory';
+
 
 
 function Navbar() {
     const [user, loading, error] = useAuthState(auth);
 
     return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex align='center'>
+        <Flex bg="white" height="44px" padding="6px 12px" justify={{ md: 'space-between' }}>
+            <Flex align='center' justify={{ base: '40px', md: 'auto' }} mr={{ base: 0, md: 2 }}>
                 <Image src='/images/redditFace.svg' height='30px' />
                 <Image src='/images/redditText.svg' height='46px' display={{ base: 'none', md: 'unset' }} />
             </Flex>
-            {/* <Directory /> */}
-            <SearchInput />
+            {user && (<Directory />)}
+            <SearchInput user={user} />
             <RightContent user={user} />
         </Flex>
     )
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,9 +1,13 @@
 import { SearchIcon } from '@chakra-ui/icons'
 import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import { User } from 'firebase/auth'
 
-function SearchInput() {
+interface SearchInputProps {
+    user?: User | null
+}
+function SearchInput({ user }: SearchInputProps) {
     return (
-        <Flex flexGrow={1} mr={2} align='center'>
+        <Flex flexGrow={1} maxWidth={user ? 'auto' : '600px'} mr={2} align='center'>
             <InputGroup>
                 <InputLeftElement
                     pointerEvents='none'
